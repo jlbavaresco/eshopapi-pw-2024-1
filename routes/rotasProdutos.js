89c4fc5173fdd50d390d3b7ba016c1b878222285
Jorge Luis Boeira Bavaresco <jorge@jorgedebian.ifsul>
2024-07-08 10:52:59 -0300

    API de produtos

---
+++ routes/rotasProdutos.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const { getProdutos, addProduto, updateProduto,
+     deleteProduto, getProdutoPorCodigo } = 
+    require('../controllers/produtoController');
+
+const rotasProdutos = new Router();
+
+rotasProdutos.route('/produto')
+               .get(getProdutos)
+               .post(addProduto)
+               .put(updateProduto);
+
+rotasProdutos.route('/produto/:codigo') 
+               .get(getProdutoPorCodigo)              
+               .delete(deleteProduto);
+
+module.exports = { rotasProdutos };
---
+++ usecases/produtoUseCases.js
@@ -0,0 +1,106 @@
+const { pool } = require('../config');
+const Produto = require('../entities/Produto');
+
+const getProdutosDB = async () => {
+    try {
+        const { rows } = await pool.query(`
+SELECT p.codigo as codigo, p.nome as nome, p.descricao as descricao, 
+p.quantidade_estoque as quantidade_estoque, p.ativo as ativo, 
+p.valor as valor, to_char(p.data_cadastro, 'YYYY-MM-DD') as data_cadastro, 
+p.categoria as categoria, c.nome as categoria_nome
+FROM produtos p
+join categorias c on p.categoria = c.codigo
+order by p.codigo`);
+        return rows.map((produto) =>
+            new Produto(produto.codigo, produto.nome, produto.descricao,
+                produto.quantidade_estoque, produto.ativo, produto.valor,
+                produto.data_cadastro, produto.categoria, produto.categoria_nome));
+    } catch (err) {
+        throw "Erro: " + err;
+    }
+}
+
+const addProdutoDB = async (body) => {
+    try {
+        const { nome, descricao, quantidade_estoque, ativo, valor,
+            data_cadastro, categoria } = body;
+        const results = await pool.query(`INSERT INTO produtos (nome, descricao, 
+            quantidade_estoque, ativo, valor, data_cadastro, categoria) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+        returning codigo,nome, descricao, quantidade_estoque, ativo, valor, 
+            to_char(data_cadastro, 'YYYY-MM-DD') as data_cadastro, categoria`,
+            [nome, descricao, quantidade_estoque, ativo, valor,
+                data_cadastro, categoria]);
+        const produto = results.rows[0];
+        return new Produto(produto.codigo, produto.nome, produto.descricao,
+            produto.quantidade_estoque, produto.ativo, produto.valor,
+            produto.data_cadastro, produto.categoria, "");
+    } catch (err) {
+        throw "Erro ao inserir o produto: " + err;
+    }
+}
+
+const updateProdutoDB = async (body) => {
+    try {
+        const { codigo, nome, descricao, quantidade_estoque, ativo, valor,
+            data_cadastro, categoria } = body;
+        const results = await pool.query(`UPDATE produtos set nome = $2, 
+            descricao = $3, quantidade_estoque = $4, ativo = $5, valor = $6, 
+            data_cadastro = $7, categoria = $8
+        WHERE codigo = $1 returning codigo,nome, descricao, quantidade_estoque, 
+        ativo, valor,  to_char(data_cadastro, 'YYYY-MM-DD') as data_cadastro, 
+        categoria`,
+            [codigo, nome, descricao, quantidade_estoque, ativo, valor,
+                data_cadastro, categoria]);
+        if (results.rowCount == 0) {
+            throw `Nenhum registro encontrado com o código ${codigo} para ser alterado`;
+        }
+        const produto = results.rows[0];
+        return new Produto(produto.codigo, produto.nome, produto.descricao,
+            produto.quantidade_estoque, produto.ativo, produto.valor,
+            produto.data_cadastro, produto.categoria, "");
+    } catch (err) {
+        throw "Erro ao alterar a produto: " + err;
+    }
+}
+
+const deleteProdutoDB = async (codigo) => {
+    try {
+        const results = await pool.query(`DELETE FROM produtos
+        WHERE codigo = $1`, [codigo]);
+        if (results.rowCount == 0) {
+            throw `Nenhum registro encontrado com o código ${codigo} para ser removido`;
+        } else {
+            return `Produto de código ${codigo} removido com sucesso!`;
+        }
+    } catch (err) {
+        throw "Erro ao remover a produto: " + err;
+    }
+}
+
+const getProdutoPorCodigoDB = async (codigo) => {
+    try {
+        const results = await pool.query(`SELECT p.codigo as codigo, 
+            p.nome as nome, p.descricao as descricao, 
+p.quantidade_estoque as quantidade_estoque, p.ativo as ativo, 
+p.valor as valor, to_char(p.data_cadastro, 'YYYY-MM-DD') as data_cadastro, 
+p.categoria as categoria, c.nome as categoria_nome
+FROM produtos p
+join categorias c on p.categoria = c.codigo
+        WHERE p.codigo = $1`, [codigo]);
+        if (results.rowCount == 0) {
+            throw `Nenhum registro encontrado com o código ${codigo}`;
+        } else {
+            const produto = results.rows[0];
+            return new Produto(produto.codigo, produto.nome, produto.descricao,
+                produto.quantidade_estoque, produto.ativo, produto.valor,
+                produto.data_cadastro, produto.categoria, "");
+        }
+    } catch (err) {
+        throw "Erro ao recuperar a produto: " + err;
+    }
+}
+
+module.exports = {
+    getProdutosDB, addProdutoDB, updateProdutoDB, deleteProdutoDB, getProdutoPorCodigoDB
+}
